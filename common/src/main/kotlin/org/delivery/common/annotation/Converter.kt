11060b219acbb9ab7154d781e2aa94019f77b02b
Develing1991 <completed0728@gmail.com>
2023-12-02 03:04:55 +0900

    refactor: custom annotation java to kotlin - @Service, @AliasFor를 사용하기 위해 treeshaking - AliasFor 사용 차이점

---
+++ common/src/main/kotlin/org/delivery/common/annotation/Converter.kt
@@ -0,0 +1,15 @@
+package org.delivery.common.annotation
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Service
+
+// 순수 java, kotlin
+// @Service, @AliasFor
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Service
+annotation class Converter(
+    @get:AliasFor(annotation = Service::class)
+    val value: String = ""
+)
