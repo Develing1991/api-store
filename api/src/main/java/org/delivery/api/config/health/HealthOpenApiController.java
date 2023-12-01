0bce7d87c5b8ba48bd0ee9ead810e887c5630fcf
Develing1991 <completed0728@gmail.com>
2023-12-02 00:25:15 +0900

    refactor: HealthOpenApiController java to kotlin

--- api/src/main/java/org/delivery/api/config/health/HealthOpenApiController.java
+++ api/src/main/java/org/delivery/api/config/health/HealthOpenApiController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
 @Slf4j
 @RestController
 @RequestMapping("/open-api")
@@ -20,3 +21,4 @@ public class HealthOpenApiController {
         log.info("health call");
     }
 }
+*/
