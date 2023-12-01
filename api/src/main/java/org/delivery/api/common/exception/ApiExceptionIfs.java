0a5a42e7bfa8ed645df3d8bd7df5a801a3a30c2a
Develing1991 <completed0728@gmail.com>
2023-12-02 02:07:50 +0900

    refactor: ApiExceptionIfs java to kotlin

--- api/src/main/java/org/delivery/api/common/exception/ApiExceptionIfs.java
+++ api/src/main/java/org/delivery/api/common/exception/ApiExceptionIfs.java
@@ -1,6 +1,7 @@
-package org.delivery.api.common.exception;
+package org.delivery.common.exception;
 
-import org.delivery.api.common.error.ErrorCodeIfs;
+
+import org.delivery.common.error.ErrorCodeIfs;
 
 public interface ApiExceptionIfs {
     ErrorCodeIfs getErrorCodeIfs();
