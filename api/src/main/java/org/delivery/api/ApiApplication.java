1c8150ff338705682289c2b9b12ad8f8bea050f3
Develing1991 <completed0728@gmail.com>
2023-12-01 23:27:02 +0900

    refactor: ApiApplication.java to ApiApplication.kt

--- api/src/main/java/org/delivery/api/ApiApplication.java
+++ api/src/main/java/org/delivery/api/ApiApplication.java
@@ -4,9 +4,13 @@ package org.delivery.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// move to kotlin
+
+/*
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args){ SpringApplication.run(ApiApplication.class, args); }
 
 }
+*/
