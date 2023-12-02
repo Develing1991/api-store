06da3e689764320fc6d61f6c03116f74d090ef92
Develing1991 <completed0728@gmail.com>
2023-12-02 21:33:03 +0900

    refactor: edit converter user null elvis

--- api/src/main/kotlin/org/delivery/api/domain/userorder/converter/UserOrderConverter.kt
+++ api/src/main/kotlin/org/delivery/api/domain/userorder/converter/UserOrderConverter.kt
@@ -13,7 +13,7 @@ import java.math.BigDecimal
 class UserOrderConverter {
 
     fun toEntity(
-        user: User,
+        user: User?,
         storeMenuEntityList: List<StoreMenuEntity>?,
         storeEntity: StoreEntity?,
     ):UserOrderEntity{
