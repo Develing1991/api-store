50edeb3a05d42f0b496fdff5d88947afaa160662
Develing1991 <completed0728@gmail.com>
2023-12-02 09:38:42 +0900

    reactor: StoreUserRepository java to kotlin

--- db/src/main/java/org/delivery/db/storeuser/StoreUserRepository.java
+++ db/src/main/java/org/delivery/db/storeuser/StoreUserRepository.java
@@ -1,5 +1,6 @@
 package org.delivery.db.storeuser;
 
+/*
 import org.delivery.db.storeuser.enums.StoreUserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,3 +11,4 @@ public interface StoreUserRepository extends JpaRepository<StoreUserEntity, Long
     // select * from store_user where email = ? and status = ? order by id desc limit 1;
     Optional<StoreUserEntity> findFirstByEmailAndStatusOrderByIdDesc(String email, StoreUserStatus status);
 }
+*/
